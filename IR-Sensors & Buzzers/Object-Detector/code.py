e3968c5acf96d2bae6f8a056d1943716720695b7
Tejovrusha <tejovrusha416@gmail.com>
2026-06-29 13:32:53 +0530

    Create code.py

---
+++ IR-Sensors & Buzzers/Object-Detector/code.py
@@ -0,0 +1,16 @@
+from machine import Pin
+import time
+
+sensor=Pin(15, Pin.IN)
+buzzer=Pin(23, Pin.OUT)
+buzzer.value(1)
+previous=current=1
+
+while True:
+    current=sensor.value()
+    if current==0 and previous==1:
+        buzzer.value(0)
+        time.sleep(0.02)
+        buzzer.value(1)
+    previous=current
+    time.sleep(0.5)
